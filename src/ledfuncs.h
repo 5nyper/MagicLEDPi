3df2ad48142105a0561f4588b69d24a89463752f
Sinatra <Vikaton@users.noreply.github.com>
2015-12-08 09:11:22 -0500

    add RGB macros

--- src/ledfuncs.h
+++ src/ledfuncs.h
@@ -2,9 +2,13 @@
 #define LEDFUNCS_H_
 
 #define LOW_FADE 5
+#define RED 4
+#define GREEN 1
+#define BLUE 5
 
 void two(int value);
 void fade(int value);
-void steady(int value);
+void steady(char* index);
+void showcase(int value);
 
 #endif
