9b88ad86074f028bc1061963ca0b7328aca4a3d9
Sinatra <Vikaton@users.noreply.github.com>
2015-12-08 09:10:26 -0500

    handle SIGINT and correspond with RGB LED

--- src/led.c
+++ src/led.c
@@ -3,13 +3,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "ledfuncs.h"
+#include <signal.h>
+#include <unistd.h>
+
+void onExit(int sig)
+{
+  if (sig == SIGINT)
+	softPwmWrite(RED, 0);
+	system("gpio write 4 0");
+   	softPwmWrite(GREEN, 0);
+	system("gpio write 1 0");
+	softPwmWrite(BLUE, 0);
+	system("gpio write 5 0");
+	exit(0);
+}
 
 int main(int argc, char* argv[]) { 
+	if (signal(SIGINT, onExit) == SIG_ERR)
+  		;
 	wiringPiSetup();
-	softPwmCreate(0, 0, 100);
+	softPwmCreate(RED, 0, 100);
+	softPwmCreate(GREEN, 0, 100);
+	softPwmCreate(BLUE, 0, 100);
 	switch (atoi(argv[1])) {
 		case 0:
-			steady(50);
+			showcase(100);
 			break;		
 		case 1:
 			two(50);
