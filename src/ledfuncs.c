5b4e5da906060396a6fd0254bd905eaf104a9e20
Sinatra <Vikaton@users.noreply.github.com>
2015-12-08 09:10:53 -0500

    Update ledfuncs.c

--- src/ledfuncs.c
+++ src/ledfuncs.c
@@ -1,6 +1,7 @@
 #include "ledfuncs.h"
 #include <wiringPi.h>
 #include <softPwm.h>
+#include <stdlib.h>
 
 void two(int value) {
 	while(1) {
@@ -19,17 +20,54 @@ void fade(int value) {
 	int x;
 	while(1) {
 		for (x = LOW_FADE; x<value; x++) {
-			softPwmWrite(0, x);
+			softPwmWrite(RED, x);
+			softPwmWrite(GREEN, x);
+			softPwmWrite(BLUE, x);
 			delay(10);
 		}
 		for (x = value; x>LOW_FADE; x--) {
-			softPwmWrite(0, x);
+			softPwmWrite(RED, x);
+			softPwmWrite(GREEN, x);
+			softPwmWrite(BLUE, x);
 			delay(10);
 		}
 	}
 }
-void steady(int value) {
+void steady(char* index) {
 	while(1) {
-		softPwmWrite(0, value);
+//atoi(&index[2])
+		softPwmWrite(RED, 100);
+		softPwmWrite(GREEN, 20);
+		softPwmWrite(BLUE, 0);
 	}
 }
+void showcase(int value) {
+	softPwmWrite(RED, value);
+	delay(1000);
+	softPwmWrite(RED, 0);
+	softPwmWrite(GREEN, value);
+	delay(1000);
+	softPwmWrite(GREEN, 0);
+	softPwmWrite(BLUE, value);
+	delay(1000);
+	softPwmWrite(BLUE, 0);
+	while (1) {
+		softPwmWrite(RED, 50);
+		softPwmWrite(GREEN, 1);   // PURPLE
+		softPwmWrite(BLUE, 70);
+		delay(200);
+		softPwmWrite(RED, 100);
+		softPwmWrite(GREEN, 5);   // ORANGE
+		softPwmWrite(BLUE, 0);
+		delay(200);
+		softPwmWrite(RED, 100);
+		softPwmWrite(GREEN, 100);   // WHITE
+		softPwmWrite(BLUE, 90);
+		delay(200);
+		softPwmWrite(RED, 15);
+		softPwmWrite(GREEN, 0);   // PINK
+		softPwmWrite(BLUE, 70);
+		delay(200);
+	}
+}
+	
