c2979065bc3d64962f43b5be141a92922721ae22
Sinatra <root@stackin.money>
2016-03-30 19:26:27 -0400

    inherit correclty

--- src/led.ino
+++ src/led.ino
@@ -12,8 +12,50 @@ enum Color {
   white
 };
 
-class Headlight {
-  friend class Both;  // this class is for singling out either left or right headlight
+class Both { // this is for using them both at the same time.
+  public:
+    Both(int lef, int righ);
+    virtual void setColor(int col);
+    void init();
+    void setBright(int x);
+    Adafruit_NeoPixel headL;
+    Adafruit_NeoPixel headR;
+  protected:
+    uint32_t colors[7] = {headL.Color(255,0,0),
+                    headL.Color(0,255,0),
+                    headL.Color(0,0,255),
+                    headL.Color(160,32,240),
+                    headL.Color(255,192,203),
+                    headL.Color(255,165,0),
+                    headL.Color(255,255,255)};
+    int color = red;
+};
+
+Both::Both(int lef, int righ) {
+  headL = Adafruit_NeoPixel(24, lef, NEO_GRB + NEO_KHZ800);
+  headR = Adafruit_NeoPixel(24, righ, NEO_GRB + NEO_KHZ800);
+}
+void Both::setColor(int col) {
+  color = col;
+  for (int i = 0; i<=24; i++) {
+    headL.setPixelColor(i, colors[color]);
+    headR.setPixelColor(i, colors[color]);
+    headL.show();
+    headR.show();
+  }
+}
+void Both::init() {
+  headL.begin();
+  headL.show();
+  headR.begin();
+  headR.show();
+}
+void Both::setBright(int x) {
+  headL.setBrightness(x);
+  headR.setBrightness(x);
+}
+
+class Headlight : public Both {
   public:
     Adafruit_NeoPixel ring;
     Headlight(int pin);
@@ -21,18 +63,9 @@ class Headlight {
     virtual void setColor(int col);
     void fade();
     void heartbeat();
-  protected:
-     uint32_t colors[7] = {ring.Color(255,0,0),
-                     ring.Color(0,255,0),
-                     ring.Color(0,0,255),
-                     ring.Color(160,32,240),
-                     ring.Color(255,192,203),
-                     ring.Color(255,165,0),
-                     ring.Color(255,255,255)};
-     int color = red;
 };
 
-Headlight::Headlight(int pin) {
+Headlight::Headlight(int pin) : Both(pin, 0) {
   ring = Adafruit_NeoPixel(24, pin, NEO_GRB + NEO_KHZ800);
 }
 
@@ -79,51 +112,21 @@ void Headlight::heartbeat() {
   delay(500);
 }
 
-class Both { // this is for using them both at the same time.
-  public:
-    Both(int lef, int righ) {
-      headL = Adafruit_NeoPixel(24, lef, NEO_GRB + NEO_KHZ800);
-      headR = Adafruit_NeoPixel(24, righ, NEO_GRB + NEO_KHZ800);
-    }
-    void setColor(int col) {
-      color = col;
-      for (int i = 0; i<=24; i++) {
-        headL.setPixelColor(i, colors[color]);
-        headR.setPixelColor(i, colors[color]);
-        headL.show();
-        headR.show();
-      }
-    }
-    void init() {
-      headL.begin();
-      headL.show();
-      headR.begin();
-      headR.show();
-    }
-    void setBright(int x) {
-      headL.setBrightness(x);
-      headR.setBrightness(x);
-    }
-    Adafruit_NeoPixel headL;
-    Adafruit_NeoPixel headR;
-  protected:
-    uint32_t colors[7] = {headL.Color(255,0,0),
-                    headL.Color(0,255,0),
-                    headL.Color(0,0,255),
-                    headL.Color(160,32,240),
-                    headL.Color(255,192,203),
-                    headL.Color(255,165,0),
-                    headL.Color(255,255,255)};
-    int color = red;
-};
-
 Both ledring(6,7);
+Headlight left(7);
 
 void setup() {
+  left.ring.begin();
+  left.ring.show();
+  left.ring.setBrightness(BRIGHTNESS);
   ledring.init();
   ledring.setBright(BRIGHTNESS);
 }
 
 void loop() {
-  ledring.setColor(green);
+  //ledring.setColor(green);
+  left.setColor(red);
+  delay(5000);
+  ledring.setColor(blue);
+  delay(5000);
 }
